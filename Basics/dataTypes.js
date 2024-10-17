912694471e7e6a201c2641a1afc8803bb0bc991f
Om Bhor <132265077+kenzo0p@users.noreply.github.com>
2024-10-17 15:47:30 +0000

    datatypes

---
+++ Basics/dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+// alert(3+3); // we are using node js not browser
+
+
+console.log(3+3);
+
+let name = "om";
+let age =18;
+let isLoggedIn = false;
+
+
+//primitive data type
+// number => 2 to power 53
+//bigint
+
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => value is not assigned yet 
+//symbol =>unique
+
+//object
+
+console.log(typeof null);//type is object
+console.log(typeof undefined);//type is undefined
+

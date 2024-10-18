3ee0433f8e5e04361c2937dbd1076b19ebe6d264
Om Bhor <132265077+kenzo0p@users.noreply.github.com>
2024-10-18 07:43:18 +0000

    new concept

--- Basics/conversionOperation.js
+++ Basics/conversionOperation.js
@@ -1,3 +1,8 @@
+
+// let str1 = "hello";
+// let str2 = " om";
+// let str3 = str1+str2;
+// console.log(str3);
 // let score =30;
 // console.log(typeof score);
 // console.log(typeof (score));
@@ -8,10 +13,39 @@
 //"" => false;
 //"om" => true;
 
-let someNumber = 33;
-let stringNumber  =String(someNumber);
-console.log(typeof stringNumber) //string 
+// let someNumber = 33;
+// let stringNumber  =String(someNumber);
+// console.log(typeof stringNumber) //string 
 
 // let isloggedIn  = "om";
 // let booleanIsLogedIn = Boolean(isloggedIn);
 // console.log(booleanIsLogedIn);
+
+
+
+/************************************* Operations *********************************/  
+
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);
+
+
+
+// console.log("1" +2);
+// console.log(1+"2");
+// console.log("1"+2+2);//122
+// console.log(1+2+"2");//1+2 =3 and by concaticating //==32
+
+
+// console.log(+true); //1
+// console.log(+""); //0
+
+
+// let num1 , num2 ,num3;
+// num1 = num2 = num3 = 2+2; 
+
+
+
+// let gameCounter =100;
+// gameCounter++;
+// console.log(gameCounter);

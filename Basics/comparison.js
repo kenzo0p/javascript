0121d74963742767788b150aba8001d26e6be397
Om Bhor <132265077+kenzo0p@users.noreply.github.com>
2024-10-18 07:59:21 +0000

    comparison basics

---
+++ Basics/comparison.js
@@ -0,0 +1,26 @@
+// console.log(2>1) //true
+// console.log(2>=1) //true
+// console.log(2<1) //false
+// console.log(2==1) //false
+// console.log(2!=1) //true
+
+
+
+
+
+// console.log("2" >1);  //js convert the 2 into number
+// console.log("02" >1); //same for this pne also
+
+// console.log(null>0);//false
+// console.log(null==0);//false
+// console.log(null>=0); //true value conversion problem the reason for is that the equality check and comparison are work differently comparison convert null to a number ,treating it as a 0 thats why 3 is true and first is false 
+
+
+
+// console.log(undefined ==0)
+// console.log(undefined >0) //it will give false in every condition
+// console.log(undefined <0)
+
+//=== strict check
+
+console.log("2" === 2);//false due to strict check it never convert number to string or string to number 

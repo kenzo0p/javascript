853161fcee4200e5945def8d59a9268569fae123
Om Bhor <132265077+kenzo0p@users.noreply.github.com>
2024-10-18 07:21:10 +0000

    conversion of data

---
+++ Basics/conversionOperation.js
@@ -0,0 +1,17 @@
+// let score =30;
+// console.log(typeof score);
+// console.log(typeof (score));
+
+//"33"  => 33
+//"33aaaabc" =>Nan
+//"true" = > 1;false => 0;
+//"" => false;
+//"om" => true;
+
+let someNumber = 33;
+let stringNumber  =String(someNumber);
+console.log(typeof stringNumber) //string 
+
+// let isloggedIn  = "om";
+// let booleanIsLogedIn = Boolean(isloggedIn);
+// console.log(booleanIsLogedIn);

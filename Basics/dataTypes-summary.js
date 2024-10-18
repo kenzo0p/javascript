3788fa0bbebf50429577506e1c1f30452864a18d
Om Bhor <132265077+kenzo0p@users.noreply.github.com>
2024-10-18 16:16:06 +0000

    data type Summary

---
+++ Basics/dataTypes-summary.js
@@ -0,0 +1,31 @@
+//primitive datatype
+// 7 types: String , Number ,Boolean , null(khali hain) , undefined ,Symbol(use for making values unique) ,BigInt
+
+
+
+//reference type(non primitive)
+//Arrays , Objects , Functions
+//js is dynamically typed language
+
+//for symbol 
+// const id = Symbol('123');
+// const anotherId =Symbol('123')
+
+// console.log(id ===anotherId)
+
+// const bigNumber = 5753453457365765766746664756746574657n;
+// console.log(typeof bigNumber)
+
+
+const heroes = ["shaktiman" , "naagraj" , "doga"];
+const obj = {
+    name:"hitesh",
+    age:"22",
+}
+
+const myfunction = function (){
+    console.log("hello world");
+};
+
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3

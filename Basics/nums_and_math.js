2245ee5eb7c27d65386ffdcff01c6bfc9c878370
Om Bhor <132265077+kenzo0p@users.noreply.github.com>
2024-10-19 09:18:37 +0000

    nums and math

---
+++ Basics/nums_and_math.js
@@ -0,0 +1,39 @@
+// const score = 400;
+// console.log(score);
+
+// const balance = new Number(100); //if we want that variable only number
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));//100.00 for adding precision value 
+// const otherNumber = 23.8966; //chekc for this also 1123.276 , 123.8885
+// console.log(otherNumber.toPrecision(3));//it returns a value in string
+
+// const hundreads = 100000;
+// console.log(hundreads.toLocaleString('en-IN')); //1,00,000 if toLocalString() it will give international standard like this 100,00.
+
+
+
+//++++++++++++++++++++++++++++++++++  MATHS  ++++++++++++++++++++++++
+
+// console.log(Math)
+
+// console.log(Math.abs(-4));//absolut value minus value will be positive
+// console.log(Math.round(4.3));//4
+// console.log(Math.ceil(4.3));//5
+// console.log(Math.floor(4.3))//4
+// console.log(Math.min(2.3,4,5));
+// console.log(Math.max(2.3,4,5));
+
+// console.log(Math.random()); //this values always between 0 and 1 any random value from 0 and 1 like 0.734687247274
+// console.log((Math.random()*10)+1);
+
+
+// //formula
+// const min =10;
+// const max =20;
+// console.log(Math.floor(Math.random() * (max-min +1)) + min)
+
+
+
+

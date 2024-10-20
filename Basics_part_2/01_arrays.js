a7535ab7f24b85315c3c729bfe6cabc4532d3344
Om Bhor <132265077+kenzo0p@users.noreply.github.com>
2024-10-20 06:27:28 +0000

    array 2

--- Basics_part_2/01_arrays.js
+++ Basics_part_2/01_arrays.js
@@ -1,11 +1,11 @@
-//array
+// array
 // const myArray = [0,1,2,3,4,5,true ,"om"];
-const myArray = [0,1,2,3,4,5];
-const myHeroes = ["om" , "saurav"];
-const myArr2 = new Array(1,2,3,4)
-// console.log(myArray[0]);
+// const myArray = [0,1,2,3,4,5];
+// const myHeroes = ["om" , "saurav"];
+// const myArr2 = new Array(1,2,3,4)
+// // console.log(myArray[0]);
 
-//array methods
+// array methods
 
 // myArray.push(6);//add from start
 // myArray.push(7);
@@ -37,13 +37,66 @@ const myArr2 = new Array(1,2,3,4)
 
 // slice and splice
 
-console.log("A" , myArray);
+// console.log("A" , myArray);
 
-const myn1 = myArray.slice(1,3);//1,2 last range will not include i.e three will not here elements will not be remove
-console.log(myn1)
-console.log("B" , myArray);
+// const myn1 = myArray.slice(1,3);//1,2 last range will not include i.e three will not here elements will not be remove
+// console.log(myn1)
+// console.log("B" , myArray);
 
 
-const myn2 = myArray.splice(1,3);//remove 1,2,3 last range will include i.e three will and also it will remove this elememts from array 
-console.log(myn2)
-console.log("c" , myArray); //0,4,5
+// const myn2 = myArray.splice(1,3);//remove 1,2,3 last range will include i.e three will and also it will remove this elememts from array 
+// console.log(myn2)
+// console.log("c" , myArray); //0,4,5
+
+
+
+
+
+
+
+
+
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++Part two ++++++++++++++++++++++++++++++++++++++++++//
+
+
+// const marvel_heroes = ["thor", "Ironman", "Spiderman"];
+// const dc_heroes = ["Superman ", "flash", "batman"];
+
+// marvel_heroes.push(dc_heroes);//concat method wiil also do the same 
+// console.log(marvel_heroes);//this will create second array as element
+// console.log(marvel_heroes[3][1]);//flash
+
+
+
+// const allHeroes = marvel_heroes.concat(dc_heroes);
+// console.log(allHeroes);
+
+
+
+// spread method  ex- glass when we drop it it will spread
+
+// const all_new_heros = [...marvel_heroes,...dc_heroes]; //spread method for concatination
+// console.log(all_new_heros);
+
+
+// console.log(...marvel_heroes);
+
+
+// const  anotherArray = [1,2,3,[4,5,6],[6,7,[4,5]]];
+// const real_another_array = anotherArray.flat(2);
+// console.log(real_another_array);
+
+
+// console.log(Array.isArray("om"));//it will print false beacuse it not an array
+// console.log(Array.from("Hitesh"));//it will give an array from method
+// console.log(Array.from({name:"hitesh"}));//it will give an empty array [] //interesting
+
+
+
+// let score1 = 100;
+// let score2 = 200;
+// let score3 = 300;
+// console.log(Array.of(score1,score2,score3)); //it will print an array [100,200,300]

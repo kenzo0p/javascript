97f46c18fc609a8d66e518e6294e6dcb6f735658
Om Bhor <132265077+kenzo0p@users.noreply.github.com>
2024-10-20 10:46:38 +0000

    destructuring of objects

--- Basics_part_2/objects_part_two.js
+++ Basics_part_2/objects_part_two.js
@@ -1,63 +1,79 @@
-// const tinder =new Object();//singleton object //constructor
-const tinderUser = {};//literal .ie literal
-tinderUser.id ="123avc";
-tinderUser.name = "om";
-tinderUser.isLoggedIn =false;
-// console.log(tinderUser)
+// // const tinder =new Object();//singleton object //constructor
+// const tinderUser = {};//literal .ie literal
+// tinderUser.id ="123avc";
+// tinderUser.name = "om";
+// tinderUser.isLoggedIn =false;
+// // console.log(tinderUser)
 
 
-const regularUser ={
-    email:"some@gmail.com",
-    fullame:{
-        userfullname:{
-            firstname:"om",
-            lastname:"bhor"
-        }
-    }
-};
-// console.log(regularUser.fullame.userfullname.firstname);
+// const regularUser ={
+//     email:"some@gmail.com",
+//     fullame:{
+//         userfullname:{
+//             firstname:"om",
+//             lastname:"bhor"
+//         }
+//     }
+// };
+// // console.log(regularUser.fullame.userfullname.firstname);
 
 
-//combining the objects 
-const obj1  ={
-    1:"a",
-    2:"b"
+// //combining the objects 
+// const obj1  ={
+//     1:"a",
+//     2:"b"
+// }
+// const obj2={
+//     3:"a",
+//     4:"b"
+// }
+
+// // const obj3 ={obj1 , obj2};//{ obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
+// // const obj3  = Object.assign({},obj1,obj2);//{ '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+// const obj3 = {...obj1 ,...obj2};
+// // console.log(obj3);//this three methods are used to combined the object but spread method always best
+
+
+// const users = [
+//     {
+//         id:"1",
+//         email:"h@gmail.com"
+//     },
+//     {
+//         id:"1",
+//         email:"a@gmail.com"
+//     },
+//     {
+//         id:"1",
+//         email:"h@gmail.com"
+//     }
+// ]
+
+
+// // console.log(users[1].email)
+
+
+// console.log(tinderUser);
+
+
+// console.log(Object.keys(tinderUser));//important it will get in array
+// console.log(Object.values(tinderUser));//important it will get in array
+// console.log(Object.entries(tinderUser));//important it will get in array //[ [ 'id', '123avc' ], [ 'name', 'om' ], [ 'isLoggedIn', false ] ]
+// console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+
+
+
+
+//objects destructuring {}
+
+
+const course ={
+    coursename : "js in hindi",
+    price :"999",
+    courseInstructor:"hitesh"
 }
-const obj2={
-    3:"a",
-    4:"b"
-}
 
-// const obj3 ={obj1 , obj2};//{ obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
-// const obj3  = Object.assign({},obj1,obj2);//{ '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
-const obj3 = {...obj1 ,...obj2};
-// console.log(obj3);//this three methods are used to combined the object but spread method always best
+// course.courseInstructor; 
 
-
-const users = [
-    {
-        id:"1",
-        email:"h@gmail.com"
-    },
-    {
-        id:"1",
-        email:"a@gmail.com"
-    },
-    {
-        id:"1",
-        email:"h@gmail.com"
-    }
-]
-
-
-// console.log(users[1].email)
-
-
-console.log(tinderUser);
-
-
-console.log(Object.keys(tinderUser));//important it will get in array
-console.log(Object.values(tinderUser));//important it will get in array
-console.log(Object.entries(tinderUser));//important it will get in array //[ [ 'id', '123avc' ], [ 'name', 'om' ], [ 'isLoggedIn', false ] ]
-console.log(tinderUser.hasOwnProperty("isLoggedIn"));
-
+const {courseInstructor:instructor} =course;
+console.log(instructor)

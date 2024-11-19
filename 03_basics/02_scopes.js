895c9bc3a7f3a47481bef1bd43d559ddae8c92b8
Om Bhor <132265077+kenzo0p@users.noreply.github.com>
2024-11-19 23:31:54 +0530

    Update 02_scopes.js

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -84,9 +84,13 @@ const addTwo = function(num){
 
 // const x = 1;
 // {
-//   console.log(x); // ReferenceError
+//   console.log(x); // ReferenceError  in ths what will happen is that when we declare const x=2 then for this const x = 1 is not accesible because of shadowing
 //   const x = 2;
 // }
 
 
-
+const x = 1;
+{
+  const x = 2; // Inner `x` declared here (shadowing the outer `x`).
+  console.log(x); // Outputs: 2 (refers to the inner `x`).
+}

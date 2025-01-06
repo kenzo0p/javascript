6124dc87fd5befa353114c0d9632ccb88b710ac4
Om Bhor <ombhor617@gmail.com>
2025-01-06 14:29:31 +0530

    find the longest word in the string

---
+++ javascript-question/Day-one.js
@@ -0,0 +1,18 @@
+const findLongestWord = (str) => {
+    if(str.trim().length === 0){
+        return false;
+    }
+    let strArr = str.split(" ")//splitting it 
+    // console.log(strArr)
+    // strArr = strArr.sort((a,b) => a.length - b.length);//sort by unicode values == ascii values
+    // // strArr = strArr.sort((a,b) => b.length - a.length);//sort by unicode values == ascii values
+    // console.log(strArr);
+    // return strArr.at(-1);//this for finding the last element in the array
+    // return strArr[0] //for first element
+
+    // another path to find it out by reduce method
+    return strArr.reduce((acc,curr) => (curr.length > acc.length ? curr : acc ), "")
+    
+
+}
+console.log(findLongestWord("Hii i am om ,I am here to guide you"));

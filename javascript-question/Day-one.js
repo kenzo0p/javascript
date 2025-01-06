57f9743aa163bfb592f8d3c222744da4fa2f9378
Om Bhor <ombhor617@gmail.com>
2025-01-06 14:32:24 +0530

    comment

--- javascript-question/Day-one.js
+++ javascript-question/Day-one.js
@@ -4,14 +4,14 @@ const findLongestWord = (str) => {
     }
     let strArr = str.split(" ")//splitting it 
     // console.log(strArr)
-    // strArr = strArr.sort((a,b) => a.length - b.length);//sort by unicode values == ascii values
+    strArr = strArr.sort((a,b) => a.length - b.length);//sort by unicode values == ascii values , by using length it will get like according to their length from low to high
     // // strArr = strArr.sort((a,b) => b.length - a.length);//sort by unicode values == ascii values
-    // console.log(strArr);
-    // return strArr.at(-1);//this for finding the last element in the array
+    console.log(strArr);
+    return strArr.at(-1);//this for finding the last element in the array
     // return strArr[0] //for first element
 
     // another path to find it out by reduce method
-    return strArr.reduce((acc,curr) => (curr.length > acc.length ? curr : acc ), "")
+    // return strArr.reduce((acc,curr) => (curr.length > acc.length ? curr : acc ), "")
     
 
 }

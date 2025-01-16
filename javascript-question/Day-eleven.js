167654a39b3dd113b81b77f8c8e2f6f381950e81
Om Bhor <ombhor617@gmail.com>
2025-01-16 11:53:12 +0530

    Add function to calculate the sum of digits of a number

---
+++ javascript-question/Day-eleven.js
@@ -0,0 +1,20 @@
+// function sumOfDigits(number){
+//     if(number < 0) {
+//         return;
+//     }
+//     const array = number.toString().split("").map(Number);
+//     let sum = 0;
+//     for(let i =0;i<array.length;i++){
+//         sum = sum + array[i];
+//     }
+//     return sum;
+// }
+
+
+const sumOfDigits = (num) => {
+    let arr = Array.from(String(num) ,Number);
+    console.log(arr);
+    return arr.reduce((acum , curr) => acum += curr , 0)
+}
+
+console.log(sumOfDigits(1234));

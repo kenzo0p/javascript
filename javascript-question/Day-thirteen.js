38f2c5269904efb0827312a17b48468f0d93ff36
Om Bhor <ombhor617@gmail.com>
2025-01-18 16:34:38 +0530

    Add function to count vowels in a string

---
+++ javascript-question/Day-thirteen.js
@@ -0,0 +1,27 @@
+// function countVowels(str){
+//     str  = str.toLowerCase().split("");
+//     console.log(str)
+//     let count =0 ;
+//     const vowels = str.map((vowel)=> {
+//         if(vowel === 'a' || vowel === 'e'|| vowel === 'i' || vowel === 'o' || vowel === 'u'){
+//             count ++;
+//         }
+//     })
+//     return count;
+// }
+
+
+const countVowels = (str) => {
+    let vowels = ['a','e' ,'i' , 'o' ,'u'];
+    let arr = str.split('');
+    let count = 0;
+    for(let char of arr){
+        if(vowels.includes(char.toLowerCase())){
+            count++;
+        }
+    }
+    return count;
+}
+
+
+console.log(countVowels("Hello World"));

83a9f54f5ee2ec3f899e9a9852fd8fbfa7e6091b
Om Bhor <ombhor617@gmail.com>
2025-12-29 17:31:57 +0530

    Add initial JavaScript functions and execution context notes

---
+++ main.js
@@ -0,0 +1,30 @@
+
+
+// getName();  //namaste  om
+// console.log(x); //undefined
+// console.log(getName) //[function : getName]
+
+var x = 7;
+function getName(){
+    console.log("Namaste om")
+}
+
+
+// getName(); //namaste om
+// console.log(x); // 7
+console.log(getName) //[function : getName] 1 functon
+
+
+console.log(getName2); //undef 2nd function
+// getName2(); //get name 2 is not a function
+
+var getName2 =  () => {
+    console.log("Namaste again");
+}
+
+
+//Interview tip -> 
+// Explain this thing
+
+
+
